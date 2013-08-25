2e9dd24394a97bab93cffedfa4019d5508d621a2
Tim Gong <gb2013@live.cn>
2013-08-26 02:08:32 +0800

    提交 HostsTool 源码；

---
+++ HostsTool/MainForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace HostsTool
+{
+    partial class MainForm
+    {
+        private System.Windows.Forms.Button btnOpenHosts;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ToolStripMenuItem btnSetDisable;
+        private System.Windows.Forms.ToolStripMenuItem btnSetEnable;
+        private System.Windows.Forms.ToolStripMenuItem btnSetGroupNum;
+        private System.Windows.Forms.Button btnSetNotTogether;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private string hostsFilePath = Path.Combine(Environment.SystemDirectory, @"drivers\etc\hosts");
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Panel panelButtons;
+        private System.Windows.Forms.TreeView treeView;
+
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnSetDisable = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSetEnable = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSetGroupNum = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.btnOpenHosts = new System.Windows.Forms.Button();
+            this.btnSetNotTogether = new System.Windows.Forms.Button();
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.contextMenuStrip.SuspendLayout();
+            this.panelBottom.SuspendLayout();
+            this.panelButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.AccessibleDescription = global::HostsTool.Properties.Resources.Icon;
+            this.treeView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.treeView.ContextMenuStrip = this.contextMenuStrip;
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.ItemHeight = 17;
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(542, 666);
+            this.treeView.TabIndex = 0;
+            this.treeView.DoubleClick += new System.EventHandler(this.treeView_DoubleClick);
+            this.treeView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeView_MouseDown);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnSetDisable,
+            this.btnSetEnable,
+            this.btnSetGroupNum});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(137, 70);
+            // 
+            // btnSetDisable
+            // 
+            this.btnSetDisable.Name = "btnSetDisable";
+            this.btnSetDisable.Size = new System.Drawing.Size(136, 22);
+            this.btnSetDisable.Text = "失效";
+            this.btnSetDisable.Click += new System.EventHandler(this.btnSetDisable_Click);
+            // 
+            // btnSetEnable
+            // 
+            this.btnSetEnable.Name = "btnSetEnable";
+            this.btnSetEnable.Size = new System.Drawing.Size(136, 22);
+            this.btnSetEnable.Text = "可用";
+            this.btnSetEnable.Click += new System.EventHandler(this.btnSetEnable_Click);
+            // 
+            // btnSetGroupNum
+            // 
+            this.btnSetGroupNum.Name = "btnSetGroupNum";
+            this.btnSetGroupNum.Size = new System.Drawing.Size(136, 22);
+            this.btnSetGroupNum.Text = "设置组编号";
+            this.btnSetGroupNum.Click += new System.EventHandler(this.btnSetGroupNum_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(139, 6);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(118, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "刷新显示";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.Controls.Add(this.btnOpenHosts);
+            this.panelBottom.Controls.Add(this.btnSetNotTogether);
+            this.panelBottom.Controls.Add(this.btnRefresh);
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(0, 630);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(542, 36);
+            this.panelBottom.TabIndex = 2;
+            // 
+            // btnOpenHosts
+            // 
+            this.btnOpenHosts.Location = new System.Drawing.Point(412, 6);
+            this.btnOpenHosts.Name = "btnOpenHosts";
+            this.btnOpenHosts.Size = new System.Drawing.Size(118, 23);
+            this.btnOpenHosts.TabIndex = 2;
+            this.btnOpenHosts.Text = "打开Hosts文件";
+            this.btnOpenHosts.UseVisualStyleBackColor = true;
+            this.btnOpenHosts.Click += new System.EventHandler(this.btnOpenHosts_Click);
+            // 
+            // btnSetNotTogether
+            // 
+            this.btnSetNotTogether.Location = new System.Drawing.Point(273, 6);
+            this.btnSetNotTogether.Name = "btnSetNotTogether";
+            this.btnSetNotTogether.Size = new System.Drawing.Size(118, 23);
+            this.btnSetNotTogether.TabIndex = 2;
+            this.btnSetNotTogether.Text = "设置互斥组";
+            this.btnSetNotTogether.UseVisualStyleBackColor = true;
+            this.btnSetNotTogether.Click += new System.EventHandler(this.btnSetNotTogether_Click);
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.Controls.Add(this.treeView);
+            this.panelButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelButtons.Location = new System.Drawing.Point(0, 0);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(542, 666);
+            this.panelButtons.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 666);
+            this.Controls.Add(this.panelBottom);
+            this.Controls.Add(this.panelButtons);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(550, 700);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HostsTool";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.panelBottom.ResumeLayout(false);
+            this.panelButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+    }
+}
+
---
+++ HostsTool/MainForm.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace HostsTool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BindTree()
+        {
+            this.treeView.Nodes.Clear();
+            StreamReader reader = new StreamReader(this.hostsFilePath, Encoding.GetEncoding("gb2312"));
+            string s = "";
+            TreeNode node = null;
+            TreeNode node2 = null;
+            while ((s = reader.ReadLine()) != null)
+            {
+                Match match = this.CheckIsGroupLine(s);
+                if (match != null)
+                {
+                    node = new TreeNode
+                    {
+                        Text = this.ProcessLineStr(match.Groups[0].Value),
+                        Tag = match.Groups[1].Value
+                    };
+                    this.treeView.Nodes.Add(node);
+                }
+                else
+                {
+                    node2 = new TreeNode
+                    {
+                        Text = this.ProcessLineStr(s)
+                    };
+                    if (node != null)
+                    {
+                        node2.Tag = node.Tag.ToString() + "_child";
+                        node.Nodes.Add(node2);
+                        continue;
+                    }
+                    this.treeView.Nodes.Add(node2);
+                }
+            }
+            reader.Close();
+            reader.Dispose();
+            foreach (TreeNode node3 in this.treeView.Nodes)
+            {
+                node3.ExpandAll();
+            }
+            this.treeView.SelectedNode = this.treeView.Nodes[0];
+        }
+
+        private void btnOpenHosts_Click(object sender, EventArgs e)
+        {
+            new Process { StartInfo = { FileName = "notepad.exe", Arguments = this.hostsFilePath } }.Start();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            this.BindTree();
+        }
+
+        private void btnSetDisable_Click(object sender, EventArgs e)
+        {
+            this.SetEnableOrDisable("disable", this.treeView.SelectedNode);
+            this.SaveHost();
+        }
+
+        private void btnSetEnable_Click(object sender, EventArgs e)
+        {
+            this.SetEnableOrDisable("enable", this.treeView.SelectedNode);
+            this.SaveHost();
+        }
+
+        private void btnSetGroupNum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSetNotTogether_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool CheckGroupIsDisable(TreeNodeCollection treeNodes, ref int disableNum, ref int nullNum)
+        {
+            int num = 0;
+            int num2 = 0;
+            Regex regex = new Regex(@"^\s*$");
+            Match match = null;
+            Regex regex2 = new Regex(@"^\s*#+");
+            Match match2 = null;
+            foreach (TreeNode node in treeNodes)
+            {
+                match2 = regex2.Match(node.Text);
+                match = regex.Match(node.Text);
+                if (match2.Success)
+                {
+                    num++;
+                }
+                else if (match.Success)
+                {
+                    num2++;
+                }
+            }
+            disableNum = num;
+            nullNum = num2;
+            return ((num + num2) == treeNodes.Count);
+        }
+
+        private Match CheckIsGroupLine(string s)
+        {
+            Match match = new Regex(@"#Group_(.*?)\s(.*)").Match(s);
+            if (match.Success)
+            {
+                return match;
+            }
+            return null;
+        }
+
+        private string GetGroupNotTogether(string groupNum)
+        {
+            Match match = new Regex(@"#(\[.*\])+").Match(this.GetHostText());
+            if (!match.Success)
+            {
+                return "";
+            }
+            MatchCollection matchs = Regex.Matches(match.Groups[1].Value, @"\[(.*?)\]");
+            string str = "";
+            foreach (Match match2 in matchs)
+            {
+                str = match2.Groups[1].Value;
+                string[] strArray = str.Split(new char[] { ',' });
+                bool flag = false;
+                foreach (string str2 in strArray)
+                {
+                    if (str2 == groupNum)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    return str;
+                }
+                str = "";
+            }
+            return str;
+        }
+
+        private string GetHostText()
+        {
+            string str = "";
+            StreamReader reader = new StreamReader(this.hostsFilePath, Encoding.GetEncoding("gb2312"));
+            str = reader.ReadToEnd();
+            reader.Close();
+            reader.Dispose();
+            return str;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.BindTree();
+            this.btnSetGroupNum.Visible = false;
+            this.btnSetNotTogether.Visible = false;
+        }
+
+        private string ProcessLineStr(string s)
+        {
+            return Regex.Replace(s, @"\t+", "     ");
+        }
+
+        private void SaveHost()
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(this.hostsFilePath, false, Encoding.GetEncoding("gb2312"));
+                foreach (TreeNode node in this.treeView.Nodes)
+                {
+                    writer.WriteLine(node.Text);
+                    if (node.Nodes.Count > 0)
+                    {
+                        foreach (TreeNode node2 in node.Nodes)
+                        {
+                            writer.WriteLine(node2.Text);
+                        }
+                        continue;
+                    }
+                }
+                writer.Close();
+                writer.Dispose();
+            }
+            catch
+            {
+                MessageBox.Show("没有保存权限，保存失败");
+                this.BindTree();
+            }
+        }
+
+        private void SetEnableOrDisable(string opType, TreeNode node)
+        {
+            if (this.CheckIsGroupLine(node.Text) != null)
+            {
+                if (opType == "disable")
+                {
+                    foreach (TreeNode node2 in node.Nodes)
+                    {
+                        if (!(node2.Text.Trim() == ""))
+                        {
+                            node2.Text = Regex.Replace(node2.Text, @"^\s*#*", "#");
+                        }
+                    }
+                }
+                else if (opType == "enable")
+                {
+                    foreach (TreeNode node3 in node.Nodes)
+                    {
+                        if (!(node3.Text.Trim() == ""))
+                        {
+                            node3.Text = Regex.Replace(node3.Text, @"^\s*#+", "");
+                        }
+                    }
+                    foreach (string str2 in this.GetGroupNotTogether(node.Tag.ToString()).Split(new char[] { ',' }))
+                    {
+                        if (str2 != node.Tag.ToString())
+                        {
+                            foreach (TreeNode node4 in this.treeView.Nodes)
+                            {
+                                if (node4.Nodes.Count > 0)
+                                {
+                                    foreach (TreeNode node5 in node4.Nodes)
+                                    {
+                                        if ((node5.Tag != null) && (node5.Tag.ToString() == (str2 + "_child")))
+                                        {
+                                            this.SetEnableOrDisable("disable", node5);
+                                        }
+                                    }
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (node.Text.Trim() == "")
+                {
+                    return;
+                }
+                if (opType == "disable")
+                {
+                    node.Text = Regex.Replace(node.Text, @"^\s*#*", "#");
+                }
+                else if (opType == "enable")
+                {
+                    node.Text = Regex.Replace(node.Text, @"^\s*#+", "");
+                }
+            }
+            this.treeView.Refresh();
+        }
+
+        private void treeView_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.CheckIsGroupLine(this.treeView.SelectedNode.Text) == null)
+            {
+                if (Regex.Match(this.treeView.SelectedNode.Text, @"^\s*#+").Success)
+                {
+                    this.btnSetEnable_Click(sender, e);
+                }
+                else
+                {
+                    this.btnSetDisable_Click(sender, e);
+                }
+            }
+            else
+            {
+                int disableNum = 0;
+                int nullNum = 0;
+                if (this.CheckGroupIsDisable(this.treeView.SelectedNode.Nodes, ref disableNum, ref nullNum))
+                {
+                    this.btnSetEnable_Click(sender, e);
+                }
+                else
+                {
+                    this.btnSetDisable_Click(sender, e);
+                }
+                this.treeView.SelectedNode.Expand();
+            }
+        }
+
+        private void treeView_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Point point2 = new Point
+                {
+                    X = e.X,
+                    Y = e.Y
+                };
+                Point pt = point2;
+                TreeNode nodeAt = this.treeView.GetNodeAt(pt);
+                if (nodeAt != null)
+                {
+                    this.treeView.SelectedNode = nodeAt;
+                    Regex regex = new Regex(@"^\s*#+");
+                    if (nodeAt.Nodes.Count > 0)
+                    {
+                        int disableNum = 0;
+                        int nullNum = 0;
+                        if (this.CheckGroupIsDisable(nodeAt.Nodes, ref disableNum, ref nullNum))
+                        {
+                            this.btnSetEnable.Visible = true;
+                            this.btnSetDisable.Visible = false;
+                        }
+                        else if (disableNum == 0)
+                        {
+                            this.btnSetEnable.Visible = false;
+                            this.btnSetDisable.Visible = true;
+                        }
+                        else
+                        {
+                            this.btnSetEnable.Visible = true;
+                            this.btnSetDisable.Visible = true;
+                        }
+                    }
+                    else if (nodeAt.Text.Trim() == "")
+                    {
+                        this.btnSetEnable.Visible = false;
+                        this.btnSetDisable.Visible = false;
+                    }
+                    else if (regex.Match(nodeAt.Text).Success)
+                    {
+                        this.btnSetEnable.Visible = true;
+                        this.btnSetDisable.Visible = false;
+                    }
+                    else
+                    {
+                        this.btnSetEnable.Visible = false;
+                        this.btnSetDisable.Visible = true;
+                    }
+                }
+            }
+        }
+    }
+}
